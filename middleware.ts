ce17d10ffca17ba4b683939c01725488175416e3
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-07-07 22:18:51 -0500

    configures API route in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/api/:path*', '/:path*'],
 };
