67d23efb29669b2c27173f2a38db20fd77d13bc7
dkashif <dkashif@arizona.edu>
2024-07-08 20:50:23 -0700

    makes the quiz page and links it to the sign up page

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,12 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignUp />
+      <SignUp 
+      path="/sign-up"
+      routing="path"
+      signInUrl="/sign-in"
+      redirectUrl="/(routes)/quiz" // Redirect to the quiz page after sign-up
+      />
     </div>
   )
 }
---
+++ app/(routes)/quiz/page.tsx
@@ -0,0 +1,15 @@
+// pages/quiz.tsx
+
+import React from 'react';
+import Quiz from '@/components/quiz';
+
+const QuizPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Quiz />
+    </div>
+  );
+};
+
+export default QuizPage;
+
