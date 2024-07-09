3bd3ed3c6f88687e54f67c6dc8863b833a4e38b5
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-07-09 16:17:35 -0500

    renders TVShowCards for discover page

--- app/(routes)/discover/page.tsx
+++ app/(routes)/discover/page.tsx
@@ -1,50 +1,51 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
-import TvShowCard from "@/components/TVShowCard";
+import TVShowCard from "@/components/TVShowCard";
 import axios from "axios";
 
 const Discover: React.FC = () => {
-  const [tvShow, setTvShow] = useState<any | null>(null);
+  const [tvShows, setTvShows] = useState<any[]>([]);
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const fetchShow = async () => {
+    const fetchShows = async () => {
       setLoading(true);
       try {
-        // Fetch show with ID 2
-        const response = await axios.get(`/api/shows/2`);
-        setTvShow(response.data);
+        const response = await axios.get(`/api/shows?page=${page}&limit=20`);
+        setTvShows(prevShows => [...prevShows, ...response.data]);
       } catch (error) {
-        console.error("Error fetching show:", error);
+        console.error("Error fetching shows:", error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchShow();
-  }, []);
+    fetchShows();
+  }, [page]);
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   return (
     <div className="min-h-screen text-white flex flex-col bg-gradient-to-r from-[#1B1919] to-[#090909]">
-      <h1 className="text-4xl font-bold mb-8 text-center">TV Show Details</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {tvShow ? (
-          <TvShowCard
-            key={tvShow.id}
-            showId={tvShow.id}
+      <h1 className="text-4xl font-bold mb-8 text-center mt-8">All TV Shows</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2">
+        {tvShows.map(show => (
+          <TVShowCard
+            key={show.id}
+            showId={show.id}
           />
-        ) : (
-          <p className="text-center">No show found</p>
-        )}
+        ))}
       </div>
       <div className="flex justify-center mt-8">
         <button
-          onClick={() => setTvShow(null)} // Optional: To clear the show
-          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+          onClick={handleLoadMore}
+          className="px-4 py-2 bg-zinc-900 text-white border border-white rounded-md"
           disabled={loading}
         >
-          {loading ? "Loading..." : "Clear"}
+          {loading ? "Loading..." : "Load More"}
         </button>
       </div>
     </div>
