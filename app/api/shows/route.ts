98944dad872ba2a6eb628f5726bc319e42b6de71
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-07-09 12:33:28 -0500

    limits number of shows per page

--- app/api/shows/route.ts
+++ app/api/shows/route.ts
@@ -1,31 +1,34 @@
-//Fetch all TV shows from the database
+// Fetch all TV shows from the database
 import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
-export async function GET (
-    req: Request,
-) {
-    try {
-        const { searchParams } = new URL(req.url)
-        const genre = searchParams.get('genre') || undefined;
-        const search = searchParams.get('search') || undefined;
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const genre = searchParams.get('genre') || undefined;
+    const search = searchParams.get('search') || undefined;
 
-        const whereClause: any = {
-            ...(genre && { genres: { has: genre } }),
-            ...(search && { name: { contains: search, mode: 'insensitive' } }),
-          };
-      
+    //Limits the number of shows per page
+    const limit = parseInt(searchParams.get('limit') || '10');
+    const page = parseInt(searchParams.get('page') || '1');
 
-        const shows = await prismadb.show.findMany({
-          where: whereClause,
-          orderBy: {
-            rating: 'desc',
-          },
-        });
+    const whereClause: any = {
+      ...(genre && { genres: { has: genre } }),
+      ...(search && { name: { contains: search, mode: 'insensitive' } }),
+    };
 
-        return NextResponse.json(shows);
-    } catch (error) {
-        console.error('[SHOWS_GET]', error);
-        return new NextResponse('Internal Error!', { status: 500 });
-    }
-};
+    const shows = await prismadb.show.findMany({
+      where: whereClause,
+      orderBy: {
+        rating: 'desc',
+      },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
+
+    return NextResponse.json(shows);
+  } catch (error) {
+    console.error('[SHOWS_GET]', error);
+    return new NextResponse('Internal Error!', { status: 500 });
+  }
+}
