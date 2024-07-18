b7541463dfa3fe1147ca8dc1d8a9dfc151c5a913
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-07-17 20:41:12 -0500

    updates TVShowCard component UI and structure

--- components/TvShowCard.tsx
+++ components/TvShowCard.tsx
@@ -34,20 +34,22 @@ const TVShowCard: React.FC<TVShowCardProps> = ({ showId }) => {
   if (!show) return <p>No show data available.</p>;
 
   return (
-    <div className="relative w-60 h-80 m-4 bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <Image
-        src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${show.backgroundpath}`}
-        alt={show.name}
-        layout="fill"
-        objectFit="cover"
-        className="absolute inset-0 z-0"
-      />
-      <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+    <div className="relative w-60 m-4 bg-zinc-800 rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-opacity duration-300 hover:opacity-40">
+      <div className="relative w-full h-72">
+        <Image
+          src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${show.backgroundpath}`}
+          alt={show.name}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="p-4 text-center">
         <Link href={`/show/${showId}`} className="text-white text-lg font-semibold">
           {show.name}
         </Link>
-        <p className="text-yellow-400 text-sm">Rating: {show.rating.toFixed(1)}/10</p>
-        <p className="text-gray-300 text-sm mt-2">{show.overview}</p>
+        <p className="text-yellow-400 text-sm mt-1 font-bold text-center">
+          Rating: {show.watchlistedrating.toFixed(1)}/10
+        </p>
       </div>
     </div>
   );
