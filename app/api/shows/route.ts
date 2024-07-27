41406d176880243f92631abbbd8da1fa89714502
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-07-27 09:20:55 -0500

    forces dynamic for show routes

--- app/api/shows/route.ts
+++ app/api/shows/route.ts
@@ -3,6 +3,8 @@
 import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
