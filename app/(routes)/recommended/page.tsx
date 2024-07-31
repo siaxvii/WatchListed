b7c40bcde040d323b54bc7cdcb03c7222df966f5
Lasya Yakkala <lasya.yakkala@gmail.com>
2024-07-30 23:55:59 -0500

    recommended shows page title change

--- app/(routes)/recommended/page.tsx
+++ app/(routes)/recommended/page.tsx
@@ -1,10 +1,7 @@
 export default function Recommended() {
   return (
     <div className="min-h-screen flex flex-col text-white bg-gradient-to-r from-[#1B1919] to-[#090909]">
-      <div className="flex-grow flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold mb-4 relative">Recommended</h1>
-        <h3 className="text-3x1 font-bold mb-4 relative">Your Recommended Page</h3>
-      </div>
+        <h1 className="text-5xl font-bold mb-8 text-center mt-20">Top Picks For You</h1>
     </div>
   )
 }
